e2cb6b2ae266cb5772274abba73c89f1b9c6289d
Nico Fuchs <fuchs19635@hs-ansbach.de>
2024-08-28 19:55:47 +0200

    25

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-    const greeting = 'Hi there!';
-    const textElement = <Text>By there!</Text>
-    const name = 'Donald';
-    
-    return (
-        <View>
-            <Text style={styles.textStyle1}>This is the components screen</Text>
-            <Text>{greeting}</Text>
-            {textElement}
-            <Text style={styles.textStyle1}>Getting started with React Native</Text>
-            <Text style={styles.textStyle2}>My name is {name}</Text>
-        </View>
-    );
+  const greeting = "Hi there!";
+  const textElement = <Text>By there!</Text>;
+  const name = "Donald";
+
+  return (
+    <View>
+      <Text style={styles.textStyle1}>This is the components screen</Text>
+      <Text>{greeting}</Text>
+      {textElement}
+      <Text style={styles.textStyle1}>Getting started with React Native</Text>
+      <Text style={styles.textStyle2}>My name is {name}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    textStyle1: {
-        fontSize: 20,
-    },
-    textStyle2: {
-        fontSize: 45
-    }
+  textStyle1: {
+    fontSize: 20,
+  },
+  textStyle2: {
+    fontSize: 45,
+  },
 });
 
 export default ComponentsScreen;
